b2bb3b1fc88c79c6e5162c4d6a9192a2c304e908
Do Chau Long <dolong2110@gmail.com>
2022-08-03 17:14:46 +0700

    add unittest

--- test_utils.py
+++ test_utils.py
@@ -5,19 +5,20 @@ from utils import *
 
 class Test(TestCase):
 
-    def test_greeting(self):
-        self.fail()
-
-
-    def test_generate_hashed_password(self):
-        self.fail()
-
-
-    def test_check_password(self):
-        self.fail()
+    # def test_greeting(self):
+    #     self.fail()
+    #
+    #
+    # def test_generate_hashed_password(self):
+    #     self.fail()
+    #
+    #
+    # def test_check_password(self):
+    #     self.fail()
 
 
     def test_is_valid_name(self):
+        self.countTestCases()
         self.assertTrue(is_valid_name("long", 4))
         self.assertFalse(is_valid_name("long", 3))  # exceed length
         self.assertFalse(is_valid_name("long%", 6))  # contain special character
@@ -28,41 +29,41 @@ class Test(TestCase):
         self.assertFalse(is_valid_name("long1", 6))  # contain number
 
 
-    def test_get_temporal(self):
-        self.fail()
-
-
-    def test_is_valid_day(self):
-        self.fail()
-
-
-    def test_is_valid_phone_number(self):
-        self.fail()
-
-
-    def test_is_valid_email(self):
-        self.fail()
-
-
-    def test_is_valid_password(self):
-        self.fail()
-
-
-    def test_is_valid_account_number(self):
-        self.fail()
-
-
-    def test_get_yes_no_choice(self):
-        self.fail()
-
-
-    def test_is_valid_balance(self):
-        self.fail()
-
-
-    def test_is_valid_message(self):
-        self.fail()
-
-
-    def test_proceed_next(self):
-        self.fail()
+    # def test_get_temporal(self):
+    #     self.fail()
+    #
+    #
+    # def test_is_valid_day(self):
+    #     self.fail()
+    #
+    #
+    # def test_is_valid_phone_number(self):
+    #     self.fail()
+    #
+    #
+    # def test_is_valid_email(self):
+    #     self.fail()
+    #
+    #
+    # def test_is_valid_password(self):
+    #     self.fail()
+    #
+    #
+    # def test_is_valid_account_number(self):
+    #     self.fail()
+    #
+    #
+    # def test_get_yes_no_choice(self):
+    #     self.fail()
+    #
+    #
+    # def test_is_valid_balance(self):
+    #     self.fail()
+    #
+    #
+    # def test_is_valid_message(self):
+    #     self.fail()
+    #
+    #
+    # def test_proceed_next(self):
+    #     self.fail()
