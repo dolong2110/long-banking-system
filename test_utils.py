4487ae68675c57387218f42a8f94a42179c10e4a
tuan2610 <77404011+tuancompa2610@users.noreply.github.com>
2022-08-04 20:38:27 +0700

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -34,8 +34,17 @@ class Test(TestCase):
     #     self.fail()
     #
     #
-    # def test_is_valid_day(self):
-    #     self.fail()
+    def test_is_valid_day(self):
+        self.assertTrue(is_valid_day([26, 10, 2003], [4, 8, 2022])) 
+        self.assertTrue(is_valid_day([18, 8, 1988], [4, 8, 2022]))
+        self.assertFalse(is_valid_day([30, 2, 2018], [4, 8, 2022]))   # invalid day in that month
+        self.assertFalse(is_valid_day([31, 9, 2019], [4, 8, 2022]))   # invalid day in that month
+        self.assertFalse(is_valid_day([0, 12, 2010], [4, 8, 2022]))   # invalid day in that month
+        self.assertFalse(is_valid_day([40, 5, 1967], [4, 8, 2022]))   # invalid day in that month
+        self.assertFalse(is_valid_day([11, 15, 2008], [4, 8, 2022]))    # invalid month
+        self.assertFalse(is_valid_day([3, 30, 2004], [4, 8, 2022]))   # invalid month
+        self.aseertFalse(is_valid_day([8, 0, 2009], [4, 8, 2022]))  # invalid month
+        
     #
     #
     # def test_is_valid_phone_number(self):
